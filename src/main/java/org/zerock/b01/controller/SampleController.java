4b6fc2f2333297373bf2324de5da6206c608a725
1000y4rang <1000y4rang@hanmail.net>
2025-07-25 16:36:39 +0900

    Thymeleaf(1)

--- src/main/java/org/zerock/b01/controller/SampleController.java
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController {
@@ -12,5 +15,11 @@ public class SampleController {
     public void hello(Model model) {
         log.info("hello....");
         model.addAttribute("message", "Hello World");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        List<String> list = Arrays.asList("1", "2", "3");
+        model.addAttribute("list", list);
     }
 }
