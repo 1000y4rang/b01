24ba9a4b4dacb9e40fc18ef0f2eddcf6cd95c59b
1000y4rang <1000y4rang@hanmail.net>
2025-07-27 16:41:07 +0900

    JPA(1)

---
+++ src/main/java/org/zerock/b01/domain/Board.java
@@ -0,0 +1,27 @@
+package org.zerock.b01.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Board extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
+
