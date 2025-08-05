836cc0d805ba4dcfd934a57f9703c1c0564bb7c4
1000y4rang <1000y4rang@hanmail.net>
2025-08-05 21:46:37 +0900

    jpa(3) :  QueryDSL

--- src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 
-import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -62,7 +63,17 @@ public class BoardRepositoryTests {
 
     @Test
     public void testPaging(){
+        Pageable  pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<Board> result = boardRepository.findAll(pageable);
 
+        log.info("total count : "+result.getTotalElements());
+        log.info("total pages : "+result.getTotalPages());
+        log.info("page number : "+result.getNumber());
+        log.info("page size : "+result.getSize());
+
+        List<Board> todoList = result.getContent();
+
+        todoList.forEach(board -> log.info(board));
     }
 
 }
