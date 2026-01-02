9376fd235151613b00320112a154857fe8195d69
1000y4rang <1000y4rang@hanmail.net>
2026-01-02 14:27:12 +0900

    p.562

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -5,3 +5,19 @@ async function get1(bno)
 
     return result.data;
 }
+
+async function getList({bno, page, size, goLast})
+{
+    // 조회
+    const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}});
+
+    // 재조회 (최신순 조회)
+    if(goLast)
+    {
+        const total = result.data.total;
+        const lastPage = parseInt(Math.ceil(total / size));
+        return getList({bno:bno, page:lastPage, size:size});
+    }
+
+    return result.data;
+}
